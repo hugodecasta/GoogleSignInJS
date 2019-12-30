470478d8e7aa661745bed172b89b2184ac851c76
hugodecasta <hugodecasta@orange.fr>
2019-12-30 23:40:52 +0100

    Updated loader for MDL Spinner

--- google_signin.js
+++ google_signin.js
@@ -95,7 +95,7 @@ class GoogleSignIn {
         let signin_btn = null
         let load_icon = null
         if(draw_btn && this.profile_data == null) {
-            load_icon = $('<div>').addClass('google_loading')
+            load_icon = $(this.get_spinner()).addClass('google_loading')
             signin_btn = tthis.get_JQ_button().css('display','none')
             $('body').append(signin_btn)
             $('body').append(load_icon)
@@ -122,6 +122,10 @@ class GoogleSignIn {
                 ok(tthis.profile_data)
             })
         })
+    }
+
+    get_spinner() {
+        return '<div class="mdl-spinner mdl-js-spinner is-active is-upgraded" data-upgraded=",MaterialSpinner"><div class="mdl-spinner__layer mdl-spinner__layer-1"><div class="mdl-spinner__circle-clipper mdl-spinner__left"><div class="mdl-spinner__circle"></div></div><div class="mdl-spinner__gap-patch"><div class="mdl-spinner__circle"></div></div><div class="mdl-spinner__circle-clipper mdl-spinner__right"><div class="mdl-spinner__circle"></div></div></div><div class="mdl-spinner__layer mdl-spinner__layer-2"><div class="mdl-spinner__circle-clipper mdl-spinner__left"><div class="mdl-spinner__circle"></div></div><div class="mdl-spinner__gap-patch"><div class="mdl-spinner__circle"></div></div><div class="mdl-spinner__circle-clipper mdl-spinner__right"><div class="mdl-spinner__circle"></div></div></div><div class="mdl-spinner__layer mdl-spinner__layer-3"><div class="mdl-spinner__circle-clipper mdl-spinner__left"><div class="mdl-spinner__circle"></div></div><div class="mdl-spinner__gap-patch"><div class="mdl-spinner__circle"></div></div><div class="mdl-spinner__circle-clipper mdl-spinner__right"><div class="mdl-spinner__circle"></div></div></div><div class="mdl-spinner__layer mdl-spinner__layer-4"><div class="mdl-spinner__circle-clipper mdl-spinner__left"><div class="mdl-spinner__circle"></div></div><div class="mdl-spinner__gap-patch"><div class="mdl-spinner__circle"></div></div><div class="mdl-spinner__circle-clipper mdl-spinner__right"><div class="mdl-spinner__circle"></div></div></div></div>'
     }
 
 }
