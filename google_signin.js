98048b92fc521f227dd4019347d2e1fb28404ce0
hugodecasta <hugodecasta@orange.fr>
2020-01-13 19:00:37 +0100

    Updated GSI to use gapi

--- google_signin.js
+++ google_signin.js
@@ -2,56 +2,69 @@
 
 export default class GoogleSignIn {
 
-    constructor(CLIENT_ID=null) {
+    constructor(CLIENT_ID=null, scope='') {
         this.profile_data = null
         this.CLIENT_ID = CLIENT_ID
         this.is_init = false
+        this.scope = scope
+    }
+
+    is_connected() {
+        return this.profile_data != null
+    }
+
+    async create_script(src) {
+        return new Promise(ok=>{
+            let script = document.createElement('script');
+            $('head').append(script)
+            script.src = src
+            script.onload = ok
+        })
+    }
+
+    async get_api(api, api_key, api_name) {
+        await this.get_profile_data()
+        gapi.client.setApiKey(api_key)
+        await gapi.client.load(api)
+        return gapi.client[api_name]
     }
 
     async init_api(CLIENT_ID=null) {
         if(this.is_init)
             return
         CLIENT_ID = CLIENT_ID==null?this.CLIENT_ID:CLIENT_ID
-        let signin_meta = $('<meta>')
-        .attr('name','google-signin-client_id')
-        .attr('content',CLIENT_ID)
-        let script = document.createElement('script');
-        $('head').append(signin_meta)
-        $('head').append(script)
-        script.src = 'https://apis.google.com/js/platform.js'
-        script.onload = function(){
-            tthis.is_init = true
-        }
-
+        await this.create_script('https://apis.google.com/js/platform.js')
+        await this.create_script('https://apis.google.com/js/api.js')
         let tthis = this
-        return new Promise((ok)=>{
-            let int = setInterval(function() {
-                if(tthis.is_init == false)
-                    return
-                clearInterval(int)
+        return new Promise(ok=>{
+            gapi.load("client:auth2", async function() {
+                await gapi.auth2.init({client_id: CLIENT_ID})
+                if(await gapi.auth2.getAuthInstance().isSignedIn.get()) {
+                    tthis.on_sign_in(await gapi.auth2.getAuthInstance().currentUser.get())
+                }
+                tthis.is_init = true
                 ok()
-            })
+            });
         })
     }
 
     async disconnect() {
-        this.profile_data.profile.disconnect()
         this.profile_data = null
-        var auth2 = gapi.auth2.getAuthInstance();
-        await auth2.disconnect()
-        await auth2.signOut()
+        await gapi.auth2.getAuthInstance().signOut();
     }
 
-    get_JQ_button() {
+    get_JQ_button(callback) {
         let btn = $('<div>').attr('id','g-signin2').addClass('g-signin2')
         let tthis = this
         btn.ready(function() {
             gapi.signin2.render('g-signin2', {
+                'scope': this.scope,
                 'width': 240,
                 'height': 50,
                 'longtitle': false,
                 'onsuccess': function(profile) {
                     tthis.on_sign_in.call(tthis,profile)
+                    callback.call(tthis,tthis.profile_data)
                 },
                 'onfailure': function(fail) {
                     console.log(fail)
@@ -88,10 +101,27 @@ export default class GoogleSignIn {
 
     async get_profile_data(draw_btn=true) {
 
+        let loader = $(this.get_spinner()).addClass('google_loading')
+        $('body').append(loader)
         await this.init_api()
+        loader.remove()
+
+        if(this.is_connected()) {
+            let user_btn = this.get_user_button(this.profile_data)
+            $('body').append(user_btn)
+            return this.profile_data
+        }
 
         let tthis = this
-
+        return new Promise(ok=>{
+            let signin_btn = this.get_JQ_button(async function() {
+                signin_btn.remove()
+                let profile_data = await tthis.get_profile_data(draw_btn)
+                ok(profile_data)
+            })
+            $('body').append(signin_btn)
+        })
+/*
         let signin_btn = null
         let load_icon = null
         if(draw_btn && this.profile_data == null) {
@@ -121,7 +151,7 @@ export default class GoogleSignIn {
                 clearInterval(int)
                 ok(tthis.profile_data)
             })
-        })
+        })*/
     }
 
     get_spinner() {
